53776f6a3fd3eaeaeb9ca7b173598a754aa9c313
Works Chan <310019@163.com>
2017-03-22 21:11:51 +0800

    Create  sqrt.go

---
+++ sqrt.go
@@ -0,0 +1,23 @@
+// Get sqrt by Newton's method
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func Sqrt(x float64) float64 {
+    v := 0.000001
+    z := x
+    for p := 0.0; math.Abs(p-z) >= v; {
+        p = z
+        z = z - (z*z - x)/(2*z)        
+        
+    }
+    return z
+}
+
+func main() {
+    fmt.Println(Sqrt(3))
+    fmt.Println(math.Sqrt(3))
+}
