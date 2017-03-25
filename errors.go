cf93d6338ac28a4ba30f7c5d48343f8f0261df0d
Works Chan <310019@163.com>
2017-03-25 15:56:05 +0800

    Update errors.go

--- errors.go
+++ errors.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"math"
+    "fmt"
+    "math"
 )
 
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+    return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
 func Sqrt(x float64) (float64, error) {
-	if(x < 0) {
-		return 0, ErrNegativeSqrt(x)
+    if(x < 0) {
+        return 0, ErrNegativeSqrt(x)
 	}
-	v := 0.000001
+    v := 0.000001
     z := x
     for p := 0.0; math.Abs(p-z) >= v; {
         p = z
         z = z - (z*z - x)/(2*z)      
     }
-	return z, nil
+    return z, nil
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
+    fmt.Println(Sqrt(2))
+    fmt.Println(Sqrt(-2))
 }
 
