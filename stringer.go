3899c950ee86755afada4d4d77a114865d3ca1c3
Works Chan <310019@163.com>
2017-03-25 14:51:51 +0800

    Update stringer.go

--- stringer.go
+++ stringer.go
@@ -10,9 +10,9 @@ func (a IPAddr) String() string {
 
 func main() {
     hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
+        "loopback":  {127, 0, 0, 1},
+        "googleDNS": {8, 8, 8, 8},
+    }
     
     for name, ip := range hosts {
         fmt.Printf("%v: %v\n", name, ip)
