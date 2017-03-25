cbd6a292cdbe4476e85a7811741662eef72501a3
Works Chan <310019@163.com>
2017-03-25 14:51:27 +0800

    Update stringer.go

--- stringer.go
+++ stringer.go
@@ -13,8 +13,8 @@ func main() {
 		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
 	}
-	
-	for name, ip := range hosts {
+    
+    for name, ip := range hosts {
         fmt.Printf("%v: %v\n", name, ip)
-	}
+    }
 }
