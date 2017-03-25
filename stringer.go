baba91f7dfc342852a8e2f31200e2986c60cfa01
Works Chan <310019@163.com>
2017-03-25 14:50:47 +0800

    Update stringer.go

--- stringer.go
+++ stringer.go
@@ -5,16 +5,16 @@ import "fmt"
 type IPAddr [4]byte
 
 func (a IPAddr) String() string {
-	return fmt.Sprintf("%v.%v.%v.%v",a[0],a[1],a[2],a[3])
+    return fmt.Sprintf("%v.%v.%v.%v",a[0],a[1],a[2],a[3])
 }
 
 func main() {
-	hosts := map[string]IPAddr{
+    hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
 	}
 	
 	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
+        fmt.Printf("%v: %v\n", name, ip)
 	}
 }
