4cfc6cc4315d69f6d2240ee83ac010119758966a
Works Chan <310019@163.com>
2017-03-25 14:47:35 +0800

    Update fibonacci.go

--- fibonacci.go
+++ fibonacci.go
@@ -7,15 +7,15 @@ import "fmt"
 func fibonacci() func() int {
     prev := 1
     next := 0
-	return func() int {
-		prev, next = next, prev+next	
-		return prev
-	}
+    return func() int {
+        prev, next = next, prev+next
+        return prev
+    }
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
 }
